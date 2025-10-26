171c39078be77c82501f404376a7fcf17ac08651
Jayesh <jjadhav799@gmail.com>
2025-10-26 20:15:51 +0530

    Initial commit - containerized FinShark app

--- frontend/src/Services/AuthService.tsx
+++ frontend/src/Services/AuthService.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { handleError } from "../Helpers/ErrorHandler";
 import { UserProfileToken } from "../Models/User";
 
-const api = "http://localhost:5167/api/";
+const api = "http://host.docker.internal:5000";
 
 export const loginAPI = async (username: string, password: string) => {
   try {
